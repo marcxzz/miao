1a5e3a83ba6e06c3de4c01d6b155d80dae5480d7
Marcxzz <angionimarco2.0@gmail.com>
2025-05-26 21:08:26 +0200

    neon db form

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,19 @@
-export default function Home() {
+import { neon } from '@neondatabase/serverless';
+
+export default function Page() {
+  async function create(formData) {
+    'use server';
+    // Connect to the Neon database
+    const sql = neon(`${process.env.DATABASE_URL}`);
+    const comment = formData.get('comment');
+    // Insert the comment from the form into the Postgres database
+    await sql.query('INSERT INTO comments (comment) VALUES ($1)', [comment]);
+  }
+
   return (
-    <main>
-      <div>ziopera</div>
-    </main>
+    <form action={create}>
+      <input type="text" placeholder="write a comment" name="comment" />
+      <button type="submit">Submit</button>
+    </form>
   );
 }
